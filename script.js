d132271c655cdf6b2a8adbe8bddc3f7e3d7784b2
tolapao <62795957+XANuN@users.noreply.github.com>
2022-09-24 01:49:33 +0700

    changed

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-(function ($) { // Begin jQuery
-    $(function () { // DOM ready
-        // If a link has a dropdown, add sub menu toggle.
-        $('nav ul li a:not(:only-child)').click(function (e) {
-            $(this).siblings('.nav-dropdown').toggle();
-            // Close one dropdown when selecting another
-            $('.nav-dropdown').not($(this).siblings()).hide();
-            e.stopPropagation();
-        });
-        // Clicking away from dropdown will remove the dropdown class
-        $('html').click(function () {
-            $('.nav-dropdown').hide();
-        });
-        // Toggle open and close nav styles on click
-        $('#nav-toggle').click(function () {
-            $('nav ul').slideToggle();
-        });
-        // Hamburger to X toggle
-        $('#nav-toggle').on('click', function () {
-            this.classList.toggle('active');
-        });
-    }); // end DOM ready
-})(jQuery); // end jQuery
+$("input[type='text'], textarea").on("keyup", function () {
+    if ($(this).val() != "" && $("textarea").val() != "" && $("input[name='category']").is(":checked") == true) {
+        $("input[type='submit']").removeAttr("disabled");
+    }
+});
+
+$("input[name='category']").on("change", function () {
+    if ($(this).val() != "" && $("textarea").val() != "" && $("input[name='category']").is(":checked") == true) {
+        $("input[type='submit']").removeAttr("disabled");
+    }
+});
