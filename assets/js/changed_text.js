2efaec0237d89fa322999cd4906b78690997b405
tolapao <62795957+XANuN@users.noreply.github.com>
2023-01-20 23:22:10 +0700

    addedd

---
+++ assets/js/changed_text.js
@@ -0,0 +1,11 @@
+// change text
+
+// function myFunction() {
+//     x = document.getElementsByClassName("bot");
+//     for (var i = 0; i < x.length; i++) {
+//         x[i].innerText = "I agree to human verification";
+//     }
+
+// }
+
+// myFunction();
